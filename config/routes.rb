7f2b7cd7c7b056a2899e8bbad40a884f2ff61f71
root <root@DESKTOP-A5F1MA6>
2023-07-21 20:41:10 -0300

    Welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
 
+  get 'welcome', to: 'welcome#greeting'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'welcome#greeting'
 end
